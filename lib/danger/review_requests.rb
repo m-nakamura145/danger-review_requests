839b69dd23201d14ec7fc21c30e2fdaaf9c20da0
m-nakamura145 <masato.nakamura145@gmail.com>
2018-11-27 23:42:11 +0900

    Implement danger-review_requests

---
+++ lib/danger/review_requests.rb
@@ -0,0 +1,11 @@
+require 'danger/review_requests/version'
+
+module Danger
+  class ReviewRequests < ::Danger::Plugin
+
+    # @param [Array<String>] reviewers
+    def request(reviewers)
+      github.api.request_pull_request_review(repo, github.pr_json['number'], reviewers: reviewers)
+    end
+  end
+end
---
+++ lib/danger/review_requests/version.rb
@@ -0,0 +1,5 @@
+module Danger
+  module ReviewRequests
+    VERSION = '0.0.1'
+  end
+end
