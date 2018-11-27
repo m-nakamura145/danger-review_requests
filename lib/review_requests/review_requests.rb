577fdadacf306eaeef33003d6ecc570a0f009a37
m-nakamura145 <masato.nakamura145@gmail.com>
2018-11-28 00:36:21 +0900

    Update version

--- lib/review_requests/review_requests.rb
+++ lib/review_requests/review_requests.rb
@@ -5,5 +5,11 @@ module Danger
     def request(reviewers)
       github.api.request_pull_request_review(repo, github.pr_json['number'], reviewers: reviewers)
     end
+
+    private
+
+    def repo
+      @repo ||= github.pr_json[:base][:repo][:full_name]
+    end
   end
 end
